b6e9f833c6eb24854ad16e8092dbfa393a8d7873
Apple <apple@Apples-MacBook-Pro.local>
2013-06-19 17:18:06 -0700

    updating activerecord challenge

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,22 @@
 require_relative '../../db/config'
 
-# implement your Contact model here
+class Contact < ActiveRecord::Base
+  validates :email, :format => { :with    => /.+@.+\..{2,}/
+                                 :message => "Invalid email address." }
+  validates :phone, :format => { :with => /\d{3}.*\d{3}.*\d{4}/,
+                                 :message => "Phone number must be formatted XXX-XXX-XXXX." }
+  validates_presence_of :company
+ 
+  def name
+  	self.first_name + " " + self.last_name
+  end
+
+  def area_code
+  	temp = self.phone
+    temp.split("")
+    temp.shift(3)
+    temp.join("")
+  end
+end
+
+
--- db/migrate/20121011144238_create_contacts.rb
+++ db/migrate/20121011144238_create_contacts.rb
@@ -1,9 +1,15 @@
 require_relative '../config'
 
-# this is where you should use an ActiveRecord migration to Create Contacts.
-
 class CreateContacts < ActiveRecord::Migration
+
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table (:contacts) do |t|
+      t.string  :first_name
+      t.string  :last_name
+      t.company :company
+      t.string  :email
+      t.string  :phone
+    end
   end
+
 end
